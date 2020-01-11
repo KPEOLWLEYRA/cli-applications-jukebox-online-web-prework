dd12009bacc96a3b10080adfb8e044624ad2a6aa
Kelly Powers <kpowers.nj@gmail.com>
2020-01-11 22:27:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -50,7 +50,7 @@ def play(array)
   song_request = gets.strip
   array.map do |i|
     song_number = array.index(i) + 1 
-    if song_request == i || i.index
+    if song_request == i || song_number
       puts "Playing #{i}."
     else
       puts "Invalid input, please try again"
