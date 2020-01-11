49376f3adf50f66def4abcf3aa4b1cb7b71ea5d3
Kelly Powers <kpowers.nj@gmail.com>
2020-01-11 22:56:47 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -50,7 +50,7 @@ def play(array)
   song_request = gets.strip
   if array.include?(song_request)
     puts "Playing #{song_request}"
-  elsif (1..9).include?(song_request)
+  elsif (1..9).include?(song_request.to_i)
     index_of_song = (song_request.to_i - 1)
     puts "Playing #{array[index_of_song]}"
   else
